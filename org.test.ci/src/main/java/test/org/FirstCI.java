3c353fe06432d02a8cc110edd7528668d0e3a852
Lakskann26 <lakskann99@gmail.com>
2016-12-11 17:47:06 -0600

    Pushing the code into the firstcode branch

---
+++ org.test.ci/src/main/java/test/org/FirstCI.java
@@ -0,0 +1,12 @@
+package test.org;
+
+public class FirstCI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test this project");
+
+	}
+
+}
